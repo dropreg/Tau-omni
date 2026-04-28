5af27d94c2f6e62a8aad1886f8cb0864f03572dc
liangxiaobo <lxb1991@hotmail.com>
2026-04-28 20:09:03 +0800

    chore: add packaging config

--- recipes/qwen_sft/llama_factory_sft.sh
+++
@@ -1,9 +0,0 @@
-
-export NCCL_P2P_LEVEL=NVL
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export WANDB_DISABLED='True'
-export SWANLAB_MODE=disabled
-
-ray stop --force && ray start --head --num-cpus=64
-
-FORCE_TORCHRUN=1 llamafactory-cli train config/llamafactory_sft.yaml
